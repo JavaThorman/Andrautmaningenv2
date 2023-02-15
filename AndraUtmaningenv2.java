b34d6bbee3293f18201c0729c784c6e647622781
JavaThorman <120687540+JavaThorman@users.noreply.github.com>
2023-02-15 11:02:59 +0100

    changed

---
+++ AndraUtmaningenv2.java
@@ -0,0 +1,15 @@
+public class AndraUtmaningenv2 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 11; i++) {
+            if (i == 5) {
+                continue;
+            }
+            System.out.print("Nästa tal är: ");
+            System.out.println(i);
+        }
+
+
+    }
+}
